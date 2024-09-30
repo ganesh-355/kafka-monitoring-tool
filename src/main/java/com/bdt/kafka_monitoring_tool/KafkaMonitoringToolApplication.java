d3c3accdc8645824e358e303d6a6ac3291dd641f
ganesh-355 <ganesh.11813332@gmail.com>
2024-09-30 20:33:58 +0530

    setting up the base project

---
+++ src/main/java/com/bdt/kafka_monitoring_tool/KafkaMonitoringToolApplication.java
@@ -0,0 +1,13 @@
+package com.bdt.kafka_monitoring_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMonitoringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMonitoringToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdt/kafka_monitoring_tool/KafkaMonitoringToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdt.kafka_monitoring_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMonitoringToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
